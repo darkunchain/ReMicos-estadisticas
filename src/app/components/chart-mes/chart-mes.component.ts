3ee7a1e80cf2cda4bef2756e845594b9fb63fc66
darkunchain <luisfer74081@gmail.com>
2021-11-30 21:57:34 -0500

    graf2 service

--- src/app/components/chart-mes/chart-mes.component.ts
+++ src/app/components/chart-mes/chart-mes.component.ts
@@ -60,9 +60,10 @@ export class ChartMesComponent implements OnInit {
     const ctrlValue = this.date.value;
     ctrlValue.year(normalizedYear.year());
     this.date.setValue(ctrlValue);
-    this.getBackendService.datosPost(this.date)
+
     this.getBackendService.getGraf2().subscribe(datos =>{
       console.log('YearHandler: ', datos)
+      this.getBackendService.datosPost(this.date)
     })
     console.log('this.date:',this.date.value)
   }
@@ -72,9 +73,10 @@ export class ChartMesComponent implements OnInit {
     ctrlValue.month(normalizedMonth.month());
     this.date.setValue(ctrlValue);
     console.log('this.date:',this.date.value)
-    this.getBackendService.datosPost(this.date)
+
     this.getBackendService.getGraf2().subscribe(datos =>{
       console.log('MonthHandler: ', datos)
+      this.getBackendService.datosPost(this.date)
     })
     datepicker.close();
 
