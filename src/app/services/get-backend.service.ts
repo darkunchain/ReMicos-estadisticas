c16d63682721bd1f77f6474853872cb30b5db489
darkunchain <luisfer74081@gmail.com>
2021-11-30 22:17:06 -0500

    ng build

--- src/app/services/get-backend.service.ts
+++ src/app/services/get-backend.service.ts
@@ -8,33 +8,33 @@ import { Observable, Subject } from 'rxjs';
 })
 export class GetBackendService {
 
-  datoPost:any;
+  datoPost: any;
   private datosPost$ = new Subject<any>();
 
   constructor(private http: HttpClient) { }
 
   datosPost(datos: any) {
     this.datoPost = datos;
-    console.log('datosPost: ',this.datoPost)
+    console.log('datosPost: ', this.datoPost)
     this.datosPost$.next(datos)
   }
 
 
-  getRegistros():Observable<Cliente[]> {
+  getRegistros(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>('http://app.remicos.com.co:8081/api/registros')
 
   }
 
-  getIngresos():Observable<Cliente[]> {
+  getIngresos(): Observable<Cliente[]> {
     return this.http.get<Cliente[]>('http://app.remicos.com.co:8081/api/ingresos')
 
   }
 
-  getGraf1():Observable<any> {
+  getGraf1(): Observable<any> {
     return this.http.get<Cliente[]>('http://app.remicos.com.co:8081/api/graf1')
   }
 
-  getGraf2():Observable<any> {
-    return this.http.post<any>('http://190.248.51.114:3000/api/graf2',this.datoPost)
+  getGraf2(): Observable<any> {
+    return this.http.post<any>('http://190.248.51.114:3000/api/graf2', this.datoPost)
   }
 }
