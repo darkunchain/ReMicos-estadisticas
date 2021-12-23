86636c9d7fd3a73e56450148e359fd8be5461083
darkunchain <luisfer74081@gmail.com>
2021-12-23 13:24:43 -0500

    graf2 year

--- src/app/components/chart-mes/chart-mes.component.ts
+++ src/app/components/chart-mes/chart-mes.component.ts
@@ -60,26 +60,15 @@ export class ChartMesComponent implements OnInit {
     const ctrlValue = this.date.value;
     ctrlValue.year(normalizedYear.year());
     this.date.setValue(ctrlValue);
-    console.log('ctrlValue:',ctrlValue,'ctrlValue.year:',ctrlValue.year,'date:', this.date)
-    /* this.getBackendService.datosPost(this.date)
-    this.getBackendService.postGraf2().subscribe(datos =>{
-      console.log('YearHandler: ', datos)
-
-    }) */
-    //console.log('this.date:',this.date.value)
+    console.log(this.date.value)
   }
 
   chosenMonthHandler(normalizedMonth: Moment, datepicker: MatDatepicker<Moment>) {
     const ctrlValue = this.date.value;
     ctrlValue.month(normalizedMonth.month());
     this.date.setValue(ctrlValue);
-    //console.log('this.date:',this.date.value)
     this.getBackendService.datosPost(this.date)
-    // this.getBackendService.postGraf2().subscribe(datos =>{
-    //   console.log('MonthHandler: ', datos)
-    // })
     datepicker.close();
-
   }
 
   ngOnInit() {
