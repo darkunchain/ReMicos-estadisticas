fc1989ed9636622d0068480417bf3b49ab792a80
darkunchain <luisfer74081@gmail.com>
2022-01-14 17:08:53 -0500

    graf1 table

--- src/app/components/my-bar-chart/my-bar-chart.component.ts
+++ src/app/components/my-bar-chart/my-bar-chart.component.ts
@@ -33,6 +33,16 @@ export class MyBarChartComponent implements OnInit {
     'rgba(255, 206, 86, 0.6)', 'rgba(255, 206, 86, 0.6)', 'rgba(255, 206, 86, 0.6)',
     'rgba(255, 206, 86, 0.6)'
   ]
+  public bar4ChartBackground: Label[] = [
+    'rgba(35, 82, 45, 0.6)', 'rgba(35, 82, 45, 0.6)', 'rgba(35, 82, 45, 0.6)',
+    'rgba(35, 82, 45, 0.6)', 'rgba(35, 82, 45, 0.6)', 'rgba(35, 82, 45, 0.6)',
+    'rgba(35, 82, 45, 0.6)'
+  ]
+  public bar5ChartBackground: Label[] = [
+    'rgba(88, 59, 25, 0.6)', 'rgba(88, 59, 25, 0.6)', 'rgba(88, 59, 25, 0.6)',
+    'rgba(88, 59, 25, 0.6)', 'rgba(88, 59, 25, 0.6)', 'rgba(88, 59, 25, 0.6)',
+    'rgba(88, 59, 25, 0.6)'
+  ]
   public barChartType: ChartType = 'bar';
   public barChartLegend = true;
   public barChartPlugins = [];
@@ -63,14 +73,26 @@ export class MyBarChartComponent implements OnInit {
           backgroundColor: this.bar3ChartBackground,
           barPercentage: 0.6
         },
+        { data: [datos.lunes15c, datos.martes15c, datos.miercoles15c,
+          datos.jueves15c, datos.viernes15c, datos.sabado15c, datos.domingo15c],
+          label: '+ 15 min',
+          backgroundColor: this.bar4ChartBackground,
+          barPercentage: 0.6
+        },
+        { data: [datos.lunes30c, datos.martes30c, datos.miercoles30c,
+          datos.jueves30c, datos.viernes30c, datos.sabado30c, datos.domingo30c],
+          label: '+ 30 min',
+          backgroundColor: this.bar5ChartBackground,
+          barPercentage: 0.6
+        },
       ];
-      this.dataTable = [datos.lunes15,datos.lunes30,datos.lunes60,
-        datos.martes15,datos.martes30,datos.martes60,
-        datos.miercoles15,datos.miercoles30,datos.miercoles60,
-        datos.jueves15,datos.jueves30,datos.jueves60,
-        datos.viernes15,datos.viernes30,datos.viernes60,
-        datos.sabado15,datos.sabado30,datos.sabado60,
-        datos.domingo15,datos.domingo30,datos.domingo60,
+      this.dataTable = [datos.lunes15,datos.lunes30,datos.lunes60,datos.lunes15c,datos.lunes30c,
+        datos.martes15,datos.martes30,datos.martes60,datos.martes15c,datos.martes30c,
+        datos.miercoles15,datos.miercoles30,datos.miercoles60,datos.miercoles15c,datos.miercoles30c,
+        datos.jueves15,datos.jueves30,datos.jueves60,datos.jueves15c,datos.jueves30c,
+        datos.viernes15,datos.viernes30,datos.viernes60,datos.viernes15c,datos.viernes30c,
+        datos.sabado15,datos.sabado30,datos.sabado60,datos.sabado15c,datos.sabado30c,
+        datos.domingo15,datos.domingo30,datos.domingo60,datos.domingo15c,datos.domingo30c,
       ]
     })
     this.getBackendService.getIngresos().subscribe(datos =>{
